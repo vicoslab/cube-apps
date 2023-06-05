8c7ee1880b59868651554fb466aabeecf5404a10
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2023-06-06 01:32:10 +0200

    Added demo application configs for board, cloth, ploscice, polyp and traffic

---
+++ ClothDemo/scene.py
@@ -0,0 +1,56 @@
+from opengl_gui.gui_components import *
+
+def get_scene(parameters):
+    
+    parameters.state.traffic_detection = 0
+
+    vicos_gray = [85.0/255.0, 85.0/255.0, 85.0/255.0, 0.75]
+    vicos_red  = [226.0/255, 61.0/255, 40.0/255.0, 0.75]
+
+    def get_docker_texture(gui: Gui, state):
+
+        echolib_handler = state.echolib_handler
+
+        if not echolib_handler.docker_channel_ready:
+            return None
+        
+        return echolib_handler.get_image() if state.traffic_detection == 1 else echolib_handler.get_camera_stream()
+
+    def toggle_detection(button: Button, gui: Gui, state):
+
+        if state.echolib_handler.docker_channel_out is not None:
+
+            toggle = button.mouse_click_count % 2
+            state.traffic_detection = toggle
+
+            if toggle == 1:
+                button.set_colour(colour = vicos_gray)
+            else:
+                button.set_colour(colour = vicos_red)
+
+            state.echolib_handler.append_command((state.echolib_handler.docker_channel_out, toggle))
+
+
+    button_scale = 1.4
+    button_detection = Button(
+        position = [0.44, 0.92],
+        scale    = [0.10*button_scale, 0.03*button_scale],
+        colour   = vicos_red,
+        on_click = toggle_detection,
+        id       = "demo_cloth_button")
+
+    button_text = TextField(
+        colour   = [1.0, 1.0, 1.0, 1.0],
+        position = [0.25, 0.65,],
+        text_scale = 0.5,
+        aspect_ratio = parameters.aspect, 
+        id = "demo_cloth_text")
+
+    button_text.set_text(font = parameters.font, text = "Vključi detekcijo")
+    button_text.center_x()
+    button_text.center_y()
+
+    button_text.depends_on(element = button_detection)
+    button_detection.center_x()
+
+    return {"get_docker_texture": get_docker_texture, "elements": [button_detection]}
---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Demo programs:
+
+## Board/Deske classification
+
+* Config and GUI: `BoardDemo`
+* Docker: `BoardDemo/docker`
+* Code repository: `https://gitlab.fri.uni-lj.si/vicos/defects/demo_cube`
+* Legacy docker: `https://gitlab.fri.uni-lj.si/vicos/defects/demo_cube/-/tree/master/dockers/DeskeDocker`
+
+## Ploscice defects
+
+* Config and GUI: `PlosciceDemo`
+* Docker: `PlosciceDemo/docker`
+* Code repository: `https://gitlab.fri.uni-lj.si/vicos/defects/demo_cube`
+* Legacy docker: `https://gitlab.fri.uni-lj.si/vicos/defects/demo_cube/-/tree/master/dockers/PlosciceDocker`
+
+## Polyp counting
+
+* Config and GUI: `PolypDemo`
+* Docker: `PolypDemo/docker`
+* Code repository: `https://gitlab.fri.uni-lj.si/vicos/defects/demo_cube`
+* Legacy docker: `https://gitlab.fri.uni-lj.si/vicos/defects/demo_cube/-/tree/master/dockers/poly_docker`
+
+## Traffic Sign detection
+
+* Config and GUI: `TrafficDemo`
+* Docker: `TrafficDemo/docker`
+* Code repository: `https://github.com/skokec/detectron-traffic-signs/`
+* Legacy docker: `https://github.com/skokec/detectron-traffic-signs/tree/villard/docker`
+
+## Cloth grasp points 
+
+* Config and GUI: `ClothDemo`
+* Docker: `https://gitlab.fri.uni-lj.si/vicos/divid/cedirnet-dev/-/tree/vicos_demo/tools/vicos_demo`
+* Code repository: `https://gitlab.fri.uni-lj.si/vicos/divid/cedirnet-dev/-/tree/vicos_demo˙
