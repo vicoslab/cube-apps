Fixed bugs in PolypDemo (wrong rotation, font color, etc)

PolypDemo/docker/scripts/run_main.py
@@ -18,8 +18,9 @@ cv2.ocl.setUseOpenCL(False)
 RESIZE_RES = (2080, 1470)
 
 DET_THICKNESS = 5
-FONT_THICKNESS = 2
+FONT_THICKNESS = 4
 FONT_SCALE = 3
+FONT_COLOR = (153, 204, 255)
 
 class PModel:
 
@@ -205,7 +206,6 @@ class PModel:
 
         image = resized_input
         
-
         D = 512
 
         xSmall = ySmall = False
@@ -243,12 +243,13 @@ class PModel:
         print("RefShape {}".format(refShape))
 
         origImage = cv2.resize(origImage, (refShape[1], refShape[0]), interpolation=cv2.INTER_AREA)
-
+        
         print("OrigImage shape {}".format(origImage.shape))
-
+    
         maskResult = self.__splitAndPredict(partsX, partsY, origImage)
         maskResult = cv2.resize(maskResult, (maskResult.shape[1] // 2, maskResult.shape[0] // 2), interpolation=cv2.INTER_AREA)
         maskResult = maskResult / np.max(maskResult)
+        
         tBoxes = self.__generate_detections_from_mask(maskResult, threshold=50)
 
         print("Found {} boxes".format(len(tBoxes)))
@@ -276,27 +277,52 @@ class PModel:
                 yd = b[1]
                 wd = b[2]
                 hd = b[3]
+                
+                # Skip detection that will be outside the bounds of the image
+                if (xd - wd // 2 >= 0) and (yd - hd // 2 >= 0) and (xd + wd // 2 < origShape[1]) and (yd + hd // 2 < origShape[0]):
+                    cv2.circle(draw_det_image, (xd, yd), radius=wd//2, color=255, thickness=DET_THICKNESS)
 
-                cv2.rectangle(draw_det_image, (xd - wd // 2, yd - hd // 2), (xd + wd // 2, yd + hd // 2), 255, DET_THICKNESS)
-                #print("Put rectangle {}{}".format(xd, yd))
+        draw_det_image = cv2.resize(draw_det_image, dsize=(final_cropped.shape[1], final_cropped.shape[0]))
+        
+        mask = np.zeros((rotated.shape[0], rotated.shape[1]), dtype=np.uint8)
+        mask[int(y_rc - h_min // 2) + final_crop:int(y_rc + h_min // 2) - final_crop, int(x_rc - w_min // 2) + final_crop:int(x_rc + w_min // 2) - final_crop] = draw_det_image
+        
+        mask = rotate_image(mask, - angle)
 
+        t = int(mask.shape[0]//2 - cutout.shape[0]// 2)
+        b = int(t + cutout.shape[0])
+        l = int(mask.shape[1]//2 - cutout.shape[1]// 2)
+        r = int(l + cutout.shape[1])
 
-        mask = np.zeros((resized_input.shape[0]+2*final_crop, resized_input.shape[1]+2*final_crop), dtype=np.uint8)
-        mask[final_crop:-final_crop, final_crop:-final_crop] = draw_det_image
+        # Check if mask is not large enough
+        if t < 0 or b > mask.shape[0] or l < 0 or r > mask.shape[1]:
+            # Calculate padding values
+            top_pad = abs(t) if t < 0 else 0
+            bottom_pad = b - mask.shape[0] if b > mask.shape[0] else 0
+            left_pad = abs(l) if l < 0 else 0
+            right_pad = r - mask.shape[1] if r > mask.shape[1] else 0
 
-        mask = rotate_image(mask, 90 - angle)
-        mask = cv2.resize(mask, dsize=(cutout.shape[1], cutout.shape[0]))
+            # Pad the mask
+            mask = cv2.copyMakeBorder(mask, top_pad, bottom_pad, left_pad, right_pad, cv2.BORDER_CONSTANT, value=0)
+
+            # Update t, b, l, r after padding
+            t += top_pad
+            b += bottom_pad
+            l += left_pad
+            r += right_pad
+
+        mask = mask[t:b, l:r]
 
         mask_full = np.zeros(image_full.shape[:2], dtype=np.uint8)
         mask_full[yc:yc + hc, xc:xc + wc] = mask
         mask_full = (mask_full > 128).astype(np.uint8)
-
+        
         mask_full3 = np.stack((mask_full, mask_full, mask_full), axis=-1)
         green = np.stack((np.zeros_like(mask_full), np.ones_like(mask_full) * 255, np.zeros_like(mask_full)), axis=-1)
 
         final = mask_full3 * green + (1 - mask_full3) * image_full
 
-        cv2.putText(final, "Stevilo detektiranih polipov je {}".format(len(tBoxes)), (300, image_full.shape[0]-300), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=FONT_SCALE, color=(0, 0, 255), thickness=FONT_THICKNESS)
+        cv2.putText(final, "Stevilo detektiranih polipov je {}".format(len(tBoxes)), (300, image_full.shape[0]-300), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=FONT_SCALE, color=FONT_COLOR, thickness=FONT_THICKNESS)
 
         return cv2.cvtColor(final, cv2.COLOR_BGR2RGB)
 
@@ -316,6 +342,7 @@ class FolderProcessing:
         for img_filename in self.img_list:
 
             frame = cv2.imread(img_filename)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             try:
                 frame = self.detection_method.predict(frame)
             except Exception as e: