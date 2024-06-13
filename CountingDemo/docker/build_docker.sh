881ee0d5c6c5dde77abcb43536d660ff34557af5
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2024-06-13 15:45:14 +0200

    Updated counting demo

--- CountingDemo/docker/build_docker.sh
+++ CountingDemo/docker/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker build . -t counting-demo:ubuntu20.04-cuda12.5.0 \
-                --build-arg CUDA_VERSION=12.5.0 \
+docker build . -t counting-demo:ubuntu20.04-cuda11.8.0-cudnn8 \
+                --build-arg CUDA_VERSION=11.8.0-cudnn8 \
                 --build-arg UBUNTU_VERSION=20.04
